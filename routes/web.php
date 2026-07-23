7c21f098900dc80b0416fbc7358d83e8808685e2
horus1196 <dcsepultura@gmail.com>
2026-07-23 13:53:00 -0600

    Endpoints para los catálogos de la base de datos así como vistas asociadas al formulario Solicitudes SISCORP

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\AutorizacionController;
+use App\Http\Controllers\ControlController;
+use App\Http\Controllers\CargoController;
+use App\Http\Controllers\UnidadAdministrativaController;
+use App\Http\Controllers\SolicitudController;
+use App\Models\Control;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +19,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::controller(SolicitudController::class)->group(function () {
+
+    Route::get('/solicitud', 'index')->name('solicitud.index');
+
+    Route::post('/solicitud/procesar', 'generarSolicitud')->name('solicitud.procesar');
+
+    Route::get("/solicitud/descargar","descargarSolicitud")->name("solicitud.descargar");
+});
+
+Route::prefix('api')->group(function () {
+
+    Route::controller(UnidadAdministrativaController::class)->group(function () {
+        Route::get('/unidades-administrativas/activas', 'activeRecords')->name('api.unidades.administrativas.activas');
+    });
+
+    Route::controller(CargoController::class)->group(function () {
+        Route::get('/cargos/activos', 'activeRecords')->name('api.cargos.activos');
+    });
+
+    Route::controller(ControlController::class)->group(function () {
+        Route::get('/control-transporte/activos', 'activeRecords')->name('api.control.transporte.activos');
+    });
+
+    Route::controller(AutorizacionController::class)->group(function () {
+        Route::post('/autorizacion-unidad-administrativa', 'autorizacionesPorUnidadAdministrativa')->name('api.autorizacion.unidad.administrativa');
+    });
 });
